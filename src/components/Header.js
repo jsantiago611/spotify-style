4e063d1e4e09a1c88287ea941abf577bb73731a5
Stackbit Code Editor <support@stackbit.com>
2021-03-11 15:38:05 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,6 +42,7 @@ export default class Header extends React.Component {
             				<Icon {...this.props} icon={'menu'} />
             				<span className="sr-only">Menu</span>
             			</button>
+                        <a></a>
             			</React.Fragment>)}
             		</nav>
             	</div>
