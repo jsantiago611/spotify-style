258f7ce98ff93cf77d02371c873cd5051d2c2a9c
Stackbit Code Editor <support@stackbit.com>
2021-03-11 15:59:41 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,7 +43,7 @@ export default class Header extends React.Component {
             				<span className="sr-only">Menu</span>
             			</button>
                         <button onclick="myFunction()">Toggle dark mode</button>
-                            <script></</script>
+                            <script></script>
             			</React.Fragment>)}
             		</nav>
             	</div>
