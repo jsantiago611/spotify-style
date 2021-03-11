396efa9e0dbeaa386309acfa0da4591336edfaab
Stackbit Code Editor <support@stackbit.com>
2021-03-11 15:56:45 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,7 @@ export default class Header extends React.Component {
                         <button onclick="myFunction()">Toggle dark mode</button>
                         <script>
                             function myFunction() {
-                            var (element) = document.body;
+                            var (--element) = document.body;
                             element.classList.toggle("dark-mode");
                             }
                             </script>
